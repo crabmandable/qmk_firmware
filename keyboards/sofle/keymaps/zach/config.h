de416903e819f22c3afdce5c501af1018c704303
Crabmandable <zach.gerstman@gmail.com>
2021-11-25 17:51:39 +0200

    Added zach sofle config

---
+++ keyboards/sofle/keymaps/zach/config.h
@@ -0,0 +1,14 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+#define EE_HANDS
+
+//Add RGB underglow
+#define RGB_DI_PIN D3
+#define RGBLED_NUM 74
+#define RGBLED_SPLIT {37,37}
+#define RGBLIGHT_ANIMATIONS
+#define RGBLIGHT_LIMIT_VAL 96 
