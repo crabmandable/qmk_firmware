2453a3c681546738f247140126e3a02cf2c7061d
Crabmandable <zach.gerstman@gmail.com>
2024-01-27 15:12:16 +0200

    Disabled LEDs for my sofle since they are borked

--- keyboards/sofle/keymaps/zach/config.h
+++ keyboards/sofle/keymaps/zach/config.h
@@ -15,8 +15,6 @@ for more options.
 //Add RGB underglow
 #define RGB_DI_PIN D3
 #define RGBLED_NUM 74
-/* #define RGBLED_SPLIT {37,37} */
 #define RGBLIGHT_ANIMATIONS
-/* #define RGBLIGHT_LIMIT_VAL 96 */
 
 #define LAYER_STATE_8BIT
